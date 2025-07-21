861d8a82fe6a3facce2aba0d922c3c54f9f48cf3
Nirav Thakor <niravthakor2002@gmail.com>
2025-07-21 15:41:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,49 +9,99 @@ const submitData = document.querySelector("#submitData");
 
 submitData.addEventListener("submit", (e) => {
 
-    
-  e.preventDefault();
 
-  const name = document.querySelector("#name").value;
-  const category = document.querySelector("#category").value;
-  const price = document.querySelector("#price").value;
-  const url = document.querySelector("#url").value;
+    e.preventDefault();
 
-  let id = productData.length + 1;
+    const name = document.querySelector("#name").value;
+    const category = document.querySelector("#category").value;
+    const price = document.querySelector("#price").value;
+    const url = document.querySelector("#url").value;
 
-  product = {
-    id,
-    name,
-    category,
-    price,
-    url,
-  };
+    let id = productData.length + 1;
 
-  productData.push(product);
+    product = {
+        id,
+        name,
+        category,
+        price,
+        url,
+    };
 
-  localStorage.setItem("productList", JSON.stringify(productData));
+    productData.push(product);
 
-  alert("Product Added.....")
+    localStorage.setItem("productList", JSON.stringify(productData));
 
-  location.reload();
+    alert("Product Added.....")
 
-  show();
+    location.reload();
+
+    show();
 });
 
 
 
-let searchItem = ""
-let sortItem = ""
+
+
+
+
+
+let searchItem = "" ;
+let sortItem = "" ;
+
+
+
+
+document.querySelector("#searchInput").addEventListener('input', (e) => {
+    console.log(e.target.value);
+    searchItem = e.target.value;
+    show() 
+    
+})
+
+
+
+document.querySelector('#sorting').addEventListener('change', (e) => {
+    sortItem = e.target.value;
+    show()
+})
+
+
+
+
+
 
 
 function show() {
 
-  let output = "";
 
-  console.log(productData);
+     const filterProduct = productData.filter((product) => {
+        return product.name.toLowerCase().includes(searchItem.toLowerCase())
+    })
 
-  productData.forEach((product) => {
-    output += `
+
+
+    filterProduct
+    .sort((a,b) => {
+        console.log(`a = ${a.mobile}, b = ${b.mobile}`);
+
+        if(sortItem == 'asc'){
+            return a.price - b.price
+        }else if(sortItem == 'desc'){
+             return b.price - a.price   
+        }
+        
+    })
+
+
+
+
+
+    let output = "";
+
+    console.log(productData);
+
+    filterProduct?.forEach((product) => {
+        output += `
 
      <div class="card col-xl-3 col-lg-3 col-md-4 m-4" style="width: 18rem;">
 
@@ -77,9 +127,9 @@ function show() {
 
     
     `;
-  });
+    });
 
-  document.querySelector("#showproduct").innerHTML = output;
+    document.querySelector("#showproduct").innerHTML = output;
 }
 
 show();
@@ -92,17 +142,17 @@ show();
 function trash(id) {
 
 
-  let filteredData = productData.filter((product) => {
-    return product.id !== id;
-  });
+    let filteredData = productData.filter((product) => {
+        return product.id !== id;
+    });
 
-  console.log(filteredData);
+    console.log(filteredData);
 
-  localStorage.setItem("productList", JSON.stringify(filteredData));
+    localStorage.setItem("productList", JSON.stringify(filteredData));
 
-  alert("Producrt Deleted...")
+    alert("Producrt Deleted...")
 
-  location.reload();
+    location.reload();
 }
 
 
@@ -114,33 +164,14 @@ function trash(id) {
 function update(id) {
 
 
-  document.querySelector("#submit").style.display = "none";
-  document.querySelector("#update").style.display = "block";
+    document.querySelector("#submit").style.display = "none";
+    document.querySelector("#update").style.display = "block";
 
 
-  const name = document.querySelector("#name");
-  const category = document.querySelector("#category");
-  const price = document.querySelector("#price");
-  const url = document.querySelector("#url");
-
-
-
-  
-
-
-
-  let updateProduct = productData.find((product) => {
-
-
-    return product.id === id;
-
-
-  });
-
-  name.value = updateProduct.name;
-  category.value = updateProduct.category;
-  price.value = updateProduct.price;
-  url.value = updateProduct.url;
+    const name = document.querySelector("#name");
+    const category = document.querySelector("#category");
+    const price = document.querySelector("#price");
+    const url = document.querySelector("#url");
 
 
 
@@ -148,45 +179,64 @@ function update(id) {
 
 
 
-  document.querySelector("#update").addEventListener("click", () => {
-
-    alert("Product Updated...")
-
-    const name = document.querySelector("#name").value;
-    const category = document.querySelector("#category").value;
-    const price = document.querySelector("#price").value;
-    const url = document.querySelector("#url").value;
+    let updateProduct = productData.find((product) => {
 
 
-     newproduct = {
+        return product.id === id;
+
+
+    });
+
+    name.value = updateProduct.name;
+    category.value = updateProduct.category;
+    price.value = updateProduct.price;
+    url.value = updateProduct.url;
+
+
+
+
+
+
+
+    document.querySelector("#update").addEventListener("click", () => {
+
+        alert("Product Updated...")
+
+        const name = document.querySelector("#name").value;
+        const category = document.querySelector("#category").value;
+        const price = document.querySelector("#price").value;
+        const url = document.querySelector("#url").value;
+
+
+        newproduct = {
             id,
             name,
             category,
             price,
             url,
-       };
+        };
 
 
 
 
-    let index = productData.findIndex((product) => {
-      return product.id === id;
+        let index = productData.findIndex((product) => {
+            return product.id === id;
+        });
+
+
+        productData[index] = newproduct
+
+        localStorage.setItem('productList', JSON.stringify(productData))
+
+
+        show();
+
+        location.reload()
+
+
     });
 
 
-    productData[index] = newproduct
 
-    localStorage.setItem('productList', JSON.stringify(productData))
-
-   
     show();
-
-    location.reload()
-
-
-  });
-
- 
-
-  show();
 }
